1c569e219e08afec553f24b60b60c76d19e3cb0a
Mayank Thakur <mayankthakur057.mt@gmail.com>
2022-02-07 23:16:22 +0530

    Did it Bro!!

--- app/src/main/java/com/example/spash_layout/MainActivity.java
+++ app/src/main/java/com/example/spash_layout/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.splash_animation);
+        setContentView(R.layout.activity_main);
 
 
     }
