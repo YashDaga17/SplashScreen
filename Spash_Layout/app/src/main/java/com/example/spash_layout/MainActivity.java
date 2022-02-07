8ffe070e886f051fbc7f3a5af14810519d6771ab
YashDaga17 <dagayash17@gmail.com>
2022-02-07 22:45:07 +0530

    Adding file

---
+++ Spash_Layout/app/src/main/java/com/example/spash_layout/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.spash_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash_animation);
+
+
+    }
+
+}
